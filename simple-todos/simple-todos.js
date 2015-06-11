4448b2b289874011cd800ca1d6ef118862768602
chiu <vchiucode@gmail.com>
2015-06-11 14:18:52 -0700

    adding user accounts

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,24 +4,24 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
 // This code only runs on the client
 // Replace the existing Template.body.helpers
-  Template.body.helpers({
-    tasks: function () {
-      if (Session.get("hideCompleted")) {
+Template.body.helpers({
+  tasks: function () {
+    if (Session.get("hideCompleted")) {
       // If hide completed is checked, filter tasks
       return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      } else {
+    } else {
       // Otherwise, return all of the tasks
       return Tasks.find({}, {sort: {createdAt: -1}});
-      }
+    }
   },
   hideCompleted: function () {
     return Session.get("hideCompleted");
   },
   // Add to Template.body.helpers
-incompleteCount: function () {
-  return Tasks.find({checked: {$ne: true}}).count();
-}
-  });
+  incompleteCount: function () {
+    return Tasks.find({checked: {$ne: true}}).count();
+  }
+});
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
 
@@ -33,14 +33,14 @@ incompleteCount: function () {
   Tasks.insert({
     text: text,
   createdAt: new Date() // current time
-  });
+});
 
   // Clear form
   event.target.text.value = "";
 
   // Prevent default form submit
   return false;
-  },
+},
 
   // Add to Template.body.events
   "change .hide-completed input": function (event) {
@@ -48,7 +48,7 @@ incompleteCount: function () {
   }
 
 
-  });
+});
 
 
   // In the client code, below everything else
@@ -56,10 +56,14 @@ incompleteCount: function () {
     "click .toggle-checked": function () {
   // Set the checked property to the opposite of its current value
   Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  },
-  "click .delete": function () {
-    Tasks.remove(this._id);
-  }
+},
+"click .delete": function () {
+  Tasks.remove(this._id);
+}
+});
+  // At the bottom of the client code
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 
 
